d6276cbef56277b08a483b6e8aad2a858da86d4f
YGNice <yg@lianhe.art>
2022-03-15 11:35:12 +0800

    refactor: remove author info

--- src/File/File.php
+++ src/File/File.php
@@ -1,12 +1,6 @@
 <?php
 
-/*
- * This file is part of the chiefgroup/laravel-esign.
- *
- * (c) peng <2512422541@qq.com>
- *
- * This source file is subject to the MIT license that is bundled.
- */
+
 
 namespace QF\LaravelEsign\File;
 
