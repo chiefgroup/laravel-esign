0e272db027c4224499130afa574e97f4630de1cd
Observer <sxpd7788+g@gmail.com>
2022-12-28 12:05:14 +0800

    fix facade

--- src/EsignFacade.php
+++ src/EsignFacade.php
@@ -3,6 +3,11 @@
 namespace QF\LaravelEsign;
 
 use Illuminate\Support\Facades\Facade;
+use QF\LaravelEsign\Account\Account;
+use QF\LaravelEsign\File\File;
+use QF\LaravelEsign\Identity\Identity;
+use QF\LaravelEsign\SignFlow\SignFlow;
+use QF\LaravelEsign\Template\Template;
 
 class EsignFacade extends Facade
 {
@@ -10,4 +15,30 @@ class EsignFacade extends Facade
     {
         return 'esign';
     }
+
+    public static function account(): Account
+    {
+        return app('esign')->account;
+    }
+
+    public static function signflow(): SignFlow
+    {
+        return app('esign')->signflow;
+    }
+
+    public static function file(): File
+    {
+        return app('esign')->file;
+    }
+
+    public static function template(): Template
+    {
+        return app('esign')->template;
+    }
+
+    public static function identity(): Identity
+    {
+        return app('esign')->identity;
+    }
+
 }
